98eac66a63bd7d84b25aecb8a0699432c691a552
Dongcp <cpdong7@gmail.com>
2023-09-01 16:14:44 +0800

    UPDATE WechatLogin

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
+    @Select("select *from user where openid = #{openid}")
+    User getByOpenid(String openid);
+
+    /**
+     * 插入数据
+     * @param user
+     */
+    void insert(User user);
+}
