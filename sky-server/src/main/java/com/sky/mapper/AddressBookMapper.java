74c4f83ab35126c233e1e5c6623b35403d7fa219
simeis147 <cpdong7@gmail.com>
2023-09-05 13:08:15 +0800

    DAY10 Task入门案例

--- sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
+++ sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
@@ -19,10 +19,10 @@ public interface AddressBookMapper {
      * @param addressBook
      */
     @Insert("insert into address_book" +
-            "        (user_id, consignee, phone, sex, province_code, province_name, city_code, city_name, district_code," +
-            "         district_name, detail, label, is_default)" +
-            "        values (#{userId}, #{consignee}, #{phone}, #{sex}, #{provinceCode}, #{provinceName}, #{cityCode}, #{cityName}," +
-            "                #{districtCode}, #{districtName}, #{detail}, #{label}, #{isDefault})")
+            " (user_id, consignee, phone, sex, province_code, province_name, city_code, city_name, district_code," +
+            "  district_name, detail, label, is_default)" +
+            " values (#{userId}, #{consignee}, #{phone}, #{sex}, #{provinceCode}, #{provinceName}, #{cityCode}, #{cityName}," +
+            "         #{districtCode}, #{districtName}, #{detail}, #{label}, #{isDefault})")
     void insert(AddressBook addressBook);
 
     /**
